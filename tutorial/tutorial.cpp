7ee96ea2dd01bb1ec49ed95b362422bc341087ed
Cj-K-7 <kllt7059@gmail.com>
2023-05-08 22:54:46 +0900

    win32 start

--- tutorial/tutorial.cpp
+++ tutorial/tutorial.cpp
@@ -1,35 +1,43 @@
 #include <windows.h>
-#include <memory>
-#include <iostream>
+#include <stdio.h>
+#include <tchar.h>
 
-
-using namespace std;
-
-int main()
+void _tmain(int argc, TCHAR* argv[])
 {
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
 
-    //two dimetional array
-    int rows, cols;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
 
-    cout << "rows , cols :";
-    cin >> rows >> cols;
-
-    int** table = new int*[rows];
-    for (int i = 0; i < rows; i++) {
-        table[i] = new int[cols];
+    if (argc != 2)
+    {
+        printf("Usage: %s [cmdline]\n", argv[0]);
+        return;
     }
 
-
-    for (int i = 0; i < rows; i++) {
-        delete[] table[i];
+    // Start the child process. 
+    if (!CreateProcess(NULL,   // No module name (use command line)
+        argv[1],        // Command line
+        NULL,           // Process handle not inheritable
+        NULL,           // Thread handle not inheritable
+        FALSE,          // Set handle inheritance to FALSE
+        0,              // No creation flags
+        NULL,           // Use parent's environment block
+        NULL,           // Use parent's starting directory 
+        &si,            // Pointer to STARTUPINFO structure
+        &pi)           // Pointer to PROCESS_INFORMATION structure
+        )
+    {
+        printf("CreateProcess failed (%d).\n", GetLastError());
+        return;
     }
 
-    //life end
-    delete[] table;
-    table = NULL;
+    // Wait until child process exits.
+    WaitForSingleObject(pi.hProcess, INFINITE);
 
-    system("pause>0");
-    return 0;
+    // Close process and thread handles. 
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
 }
-
-
